ebc1c01fcc6a3a4708418534e404b72270f37d26
JY0004 <jy0004@JY0004s-MacBook-Pro.local>
2023-12-08 18:01:06 +0530

    change url

--- server/routes/api/product.js
+++ server/routes/api/product.js
@@ -12,7 +12,7 @@ const role = require('../../middleware/role');
 const checkAuth = require('../../utils/auth');
 const path = require('path');
 const fs = require('fs');
-const baseUrl = 'https://node.jydigitek.com/';
+const baseUrl = 'https://node.jydigitek.com/public/uploads/';
 
 const {
   getStoreProductsQuery,
